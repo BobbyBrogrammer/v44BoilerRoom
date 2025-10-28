9b57abb90d56e9b1e581ffca207c633382b76aae
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-10-28 09:20:43 +0100

    commit notifications

--- src/main/java/org/example/notifications/ReceiptPrinter.java
+++ src/main/java/org/example/notifications/ReceiptPrinter.java
@@ -1,4 +1,9 @@
 package org.example.notifications;
 
 public class ReceiptPrinter {
+    public void printReceipt(double total) {
+        System.out.println("\n--- Kvitto ---");
+        System.out.println("Total: " + total + " kr");
+    }
+
 }
