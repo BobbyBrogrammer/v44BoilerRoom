3e4bce8d1ec0544d6ae69910fac5746a6c627aa9
BobbyBrogrammer <robin.lindholm111@gmail.com>
2025-10-28 10:36:57 +0100

    Merge branch 'Robin' CheckoutService

--- src/main/java/org/example/notifications/SmsSender.java
+++ src/main/java/org/example/notifications/SmsSender.java
@@ -2,8 +2,8 @@ package org.example.notifications;
 
 public class SmsSender implements MessageSender {
     @Override
-    public void send(String message) {
-        System.out.println("SMS skickad: " + message);
+    public void send(String to, String message) {
+        System.out.println("SMS skickad till: " + to + " med meddelande:  " + message);
     }
 
 }
--- src/main/java/org/example/service/CheckoutService.java
+++ src/main/java/org/example/service/CheckoutService.java
@@ -1,4 +1,43 @@
 package org.example.service;
 
+import org.example.discounts.DiscountPolicy;
+import org.example.model.Customer;
+import org.example.model.ShoppingCart;
+import org.example.notifications.ReceiptPrinter;
+
 public class CheckoutService {
+    package org.example.service;
+
+import org.example.discounts.DiscountPolicy;
+import org.example.model.Customer;
+import org.example.model.ShoppingCart;
+import org.example.notifications.ReceiptPrinter;
+
+    public class CheckoutService {
+        private final DiscountPolicy discountPolicy;
+        private final NotificationService notificationService;
+        private final ReceiptPrinter receiptPrinter;
+
+        public CheckoutService(DiscountPolicy discountPolicy, NotificationService notificationService,
+                               ReceiptPrinter receiptPrinter) {
+            this.discountPolicy = discountPolicy;
+            this.notificationService = notificationService;
+            this.receiptPrinter = receiptPrinter;
+        }
+
+        public void checkout(ShoppingCart cart, Customer customer) {
+            if (customer == null || customer.getEmail() == null || customer.getEmail().isBlank()) {
+                throw new IllegalArgumentException("Tom email adress...");
+            }
+            if (cart.gettotPrice() <= 0) {
+                throw new IllegalArgumentException("Ogiltigt pris: tom varukorg!");
+            }
+        }
+        double finalPrice = discountPolicy.applyDiscount(cart);
+    receiptPrinter.printReceipt(finalPrice);
+    notificationService.applyCustomer("Hej " + customer.getName() + "! Ditt köp gick igenom, Totalt: " +
+        finalPrice + " kr");
+    }
+
+
 }
